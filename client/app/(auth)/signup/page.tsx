5ccdfc44e3c39b6409c79432fb720961991bcb1b
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-23 20:36:28 +0530

    (fix) signup

--- client/app/(auth)/signup/page.tsx
+++ client/app/(auth)/signup/page.tsx
@@ -1,16 +1,26 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Signup from "@/components/Auth/signup.component";
 import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
 
 const SignuPage = () => {
   const router = useRouter();
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
 
-  if (isAuthenticated) {
-    router.push("/home");
-    return;
+  useEffect(() => {
+    if (isAuthenticated && !loading) {
+      router.push("/home");
+    }
+  }, [isAuthenticated, router, loading]);
+
+  if(loading) {
+    return (
+      <div className='flex items-center justify-center min-h-screen animate-spin'>
+        <Loader2 />
+      </div>
+    )
   }
 
   return (
