42b8eacd21d4331a29cb1f497feb0c822cd1e897
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-24 19:50:10 +0530

    (fix) redirection_&_user_err

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from "express";
 import userService from "../services/user.service";
+import ApiError from "../config/error";
+import { asyncMiddleware } from "../middlewares/asyncHandler";
 
-const getProfile = async (req: Request, res: Response) => {
+const getProfile = asyncMiddleware(async (req: Request, res: Response) => {
     try {
         const userId = req.userId;
         const user = await userService.getUserById(userId);
-        if(!user) throw new Error('No user was found');
+        if(!user) {
+            throw new ApiError(404, 'No user was found');
+        }
         const { password, ...userWithoutPassword } = user.toObject();
         res.status(200).send({ user: userWithoutPassword });
     } catch (error) {
-        console.log('failed to fetch profile:', error);
+        if (error instanceof ApiError) {
+            throw error;
+          }
     }
-}
+});
 
 export default { getProfile };
